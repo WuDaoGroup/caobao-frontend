13e87ebfb5e9e44c31d4e76f0b9a4e1146c4a51a
Yinghao Zhu <yhzhu99@gmail.com>
2022-03-02 16:25:15 +0800

    fix: use problem not question

--- src/routes/alg/index.svelte
+++ src/routes/alg/index.svelte
@@ -1,16 +1,16 @@
 
 <script>
     import { goto } from '$app/navigation';
-    let questionList = [
+    let problemList = [
         {
-            question: "Assignment01",
+            problem: "Assignment01",
             time: "20220301"
         },
         {
-            question: "Assignment02",
+            problem: "Assignment02",
             time: "20220302"
         }]
-    function handleClickQuestion(addr){
+    function handleClickProblem(addr){
         goto('/alg/' + addr);
 
     }
@@ -18,11 +18,11 @@
 
 <h1>算法页</h1>
 
-{#each questionList as question}
+{#each problemList as problem}
     <div>
-        <h2>{question.question}</h2>
-        <p>发布时间: {question.time}</p>
+        <h2>{problem.problem}</h2>
+        <p>发布时间: {problem.time}</p>
     </div>
-    <button class="btn btn-outline btn-sm mt-2" on:click={()=>goto('alg/'+question.question)}>查看</button>
+    <button class="btn btn-outline btn-sm mt-2" on:click={()=>goto('alg/'+problem.problem)}>查看</button>
     <div class="divider"></div> 
 {/each}
