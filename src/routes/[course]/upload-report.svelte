46aaadfdecd17fabff76406a0b3e077dc0834412
Yinghao Zhu <yhzhu99@gmail.com>
2022-03-25 13:32:23 +0800

    style: add info box

--- src/routes/[course]/upload-report.svelte
+++ src/routes/[course]/upload-report.svelte
@@ -1,6 +1,16 @@
 <h1>提交实验报告</h1>
 
-<div class="divider/"></div>
+<div class="alert shadow-lg mt-4">
+    <div>
+    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-info flex-shrink-0 w-6 h-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+    <span class="flex items-center ml-1">作业内容要求：(1) 介绍对题目的分析思路，(2) 伪代码说明实现过程，(3) 根据计算过程分析时间复杂度，需要有分析过程，不要直接给结果</span>
+    </div>
+  </div>
 
-作业内容要求：(1) 介绍对题目的分析思路，(2) 伪代码说明实现过程，(3) 根据计算过程分析时间复杂度，需要有分析过程，不要直接给结果
+<div class="mt-4">
+
+    以“墩墩数数”这题为例，点击<a href="https://www.overleaf.com/read/qcdjfyjrwjmm">参考的提交内容链接</a>查看详情。
+
+</div>
+
 
