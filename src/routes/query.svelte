85b343ec418ce929b28fc5105c4b01f22a3f4147
Yinghao Zhu <yhzhu99@gmail.com>
2022-06-10 22:13:18 +0800

    feat: init query score page

---
+++ src/routes/query.svelte
@@ -0,0 +1,27 @@
+<div class="flex flex-wrap">
+    <div class="form-control mx-4">
+        <label class="input-group">
+          <span>学号</span>
+          <input type="text" placeholder="20370000" class="input input-bordered" />
+        </label>
+    </div>
+    
+    <div class="form-control mx-4">
+        <label class="input-group">
+          <span>密码</span>
+          <input type="password" placeholder="123456" class="input input-bordered" />
+        </label>
+    </div>
+
+    <div class="form-control">
+        <div class="input-group">
+          <select class="select select-bordered">
+            <option disabled selected>选择课程</option>
+            <option>OOP</option>
+            <option>算法</option>
+          </select>
+          <button class="btn">Go</button>
+        </div>
+    </div>
+</div>
+
