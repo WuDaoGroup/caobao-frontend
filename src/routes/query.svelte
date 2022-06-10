1dd3e4b7077f8ba33fc2eaf5482b25fa7deb429d
Yinghao Zhu <yhzhu99@gmail.com>
2022-06-10 22:17:10 +0800

    feat: bind values in query score page

--- src/routes/query.svelte
+++ src/routes/query.svelte
@@ -1,26 +1,33 @@
+<script>
+	let sid;
+	let password;
+    let course;
+    $: console.log(sid, password, course)
+</script>
+
+
 <div class="flex flex-wrap">
     <div class="form-control mx-4">
         <label class="input-group">
           <span>学号</span>
-          <input type="text" placeholder="20370000" class="input input-bordered" />
+          <input type="text" placeholder="20370000" class="input input-bordered" bind:value={sid}/>
         </label>
     </div>
     
     <div class="form-control mx-4">
         <label class="input-group">
           <span>密码</span>
-          <input type="password" placeholder="123456" class="input input-bordered" />
+          <input type="password" placeholder="123456" class="input input-bordered" bind:value={password}/>
         </label>
     </div>
 
     <div class="form-control">
         <div class="input-group">
-          <select class="select select-bordered">
-            <option disabled selected>选择课程</option>
+          <select class="select select-bordered" bind:value={course}>
             <option>OOP</option>
             <option>算法</option>
           </select>
-          <button class="btn">Go</button>
+          <button class="btn">查询</button>
         </div>
     </div>
 </div>
