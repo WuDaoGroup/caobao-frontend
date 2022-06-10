032e79254697ebed0f500fbd5c3db29f4faab431
Yinghao Zhu <yhzhu99@gmail.com>
2022-06-11 03:23:36 +0800

    fix: api route

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// export const baseLink = 'https://www.yhzhu.top'
-// export const baseLink = 'http://106.54.165.117:8059'
-export const baseLink = 'http://localhost:8059'
+export const baseLink = 'https://www.yhzhu.top'
+// export const baseLink = 'http://localhost:8059'
 // export const baseLink = 'https://caobao-backend.herokuapp.com'
 
 // Create a instance of axios to use the same base url.
